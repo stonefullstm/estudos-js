174576f31eab550ab0a6cfd9b04361ff87f910a1
stonefullstm <stonefull.stm@gmail.com>
2022-04-30 23:26:24 -0300

    Novas alterações

--- Desafios/Desafiadores/desafiador-014.js
+++ Desafios/Desafiadores/desafiador-014.js
@@ -8,6 +8,21 @@ Escreva um algoritmo que some números primos de 0 à 150 e retorne o resultado.
 
 function sumPrimesNumbers(){
   // Desenvolva seu código nessa função
+  let soma = 0;
+  for (let numero = 1; numero <= 150; numero++) {
+    let primo = true;
+    let i = 2;
+    while (i <= Math.trunc(Math.sqrt(numero)) && primo) {
+      if (numero % i == 0) {
+        primo = false;
+      }
+      i++;
+    }
+    if (primo) {
+      soma += numero;
+    }
+  }
+  return soma;
 }
 
 module.exports = sumPrimesNumbers;
