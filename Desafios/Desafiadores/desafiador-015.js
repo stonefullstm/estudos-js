b5dca1de54f4d769fb371f9e004600961a803f32
stonefullstm <stonefull.stm@gmail.com>
2022-04-30 23:18:47 -0300

    Novas alterações

--- Desafios/Desafiadores/desafiador-015.js
+++ Desafios/Desafiadores/desafiador-015.js
@@ -11,10 +11,21 @@ A média deve ter até 2 casas decimais de precisão.
 
 function factorial15(number) {
   // Desenvolva aqui a lógica para calcular o fatorial
+  let fatorial = 1;
+  for (let i = 1; i <= number; i++) {
+    fatorial = fatorial * i;
+  }
+  return fatorial;
 }
 
 function meanFactorialFromAToB(a, b) {
   // Desenvolva aqui a lógica para calcular a média dos fatoriais de 0 a 5
+  let soma = 0;
+  for (let i = a; i <= b; i++) {
+    soma += factorial15(i);
+  }
+  let media = soma / (b - a + 1);
+  return media.toFixed(2);
 }
 
 module.exports = {
